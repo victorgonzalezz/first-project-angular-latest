b2b389dee9015f263ae39fee4f470c1bd758c214
Victor Gonzalez <vitorpcg@gmail.com>
2023-12-13 17:55:47 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,13 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+  <!-- <router-outlet></router-outlet> -->
+  <h1>Curso de Angular</h1>
+  <h2>Compontens</h2>
+
+  {{ title }}
+  `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
